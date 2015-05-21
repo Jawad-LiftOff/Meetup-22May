b457e34d19c439ffc83bbd7b8e49a77dcbefa42a
Jawad-LiftOff <jawad@liftoffllc.com>
2015-05-21 14:30:38 +0530

    pre link and post link included in example

--- 5.Compile-and-link/app.js
+++ 5.Compile-and-link/app.js
@@ -6,9 +6,10 @@ angular.module('compilenlink', [])
       item: '@'
     },
     compile: function(tElem,attrs) {
-      console.log('compile called for', attrs.item)
-      return function(scope,elem,attrs) {
-        console.log('link called for', attrs.item);
+      console.log('compile called for', attrs.item);
+      return {
+        pre: function(scope,elem,attrs) { console.log('pre link called for', attrs.item);},
+        post: function(scope,elem,attrs) { console.log('post link called for', attrs.item);}
       };
     },
     template: '<div>{{ item }}</div>'
